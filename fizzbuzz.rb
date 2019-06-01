cad8f80185797c30b1fdbe9ec648a518211c201b
magical-flow-3186 <rregalado17@gmail.com>
2019-06-01 01:21:44 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,14 @@
 number = ()
 
 def fizzbuzz(number)
- if number % 3 == 0 
+ if (number % 3 == 0) && (number % 5 == 0)
+   "FizzBuzz"
+  elsif number % 3 == 0 
    "Fizz"
+  elsif number % 5 == 0 
+    "Buzz"
+  else
+    NIL
  end
 end
 
